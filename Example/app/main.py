502481836fb9a4d9031bea9dcd265fcfc3ebbb7b
linmberg <l.berghaeuser@campus.tu-berlin.de>
2024-05-22 11:01:20 +0200

    unnoetigen file geloescht

--- Example/app/main.py
+++ Example/app/main.py
@@ -2,6 +2,6 @@ from .ui import setup_ui
 from .ui_alternative import setup_ui_alternative
 
 def run_app():
-    #app = setup_ui()
-    app = setup_ui_alternative()
+    app = setup_ui()
+    #app = setup_ui_alternative()
     app.mainloop()
