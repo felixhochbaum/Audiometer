6f45ec236671199cd634267131c7ca48a1e9c910
linmberg <l.berghaeuser@campus.tu-berlin.de>
2024-06-19 12:09:10 +0200

    make Audiogramm DINconf

--- app/audiogram.py
+++ app/audiogram.py
@@ -30,26 +30,34 @@ def create_audiogram(freqs, right_values, left_values, save=True):
     ax.text(52, 115, 'Hochgradige Schwerhörigkeit', ha='left', va='center', fontsize=8)
 
     # Plot der Hörschwellen
-    ax.plot(freqs, right_values, marker='o', linestyle='-', color='b', label='rechtes Ohr')
-    ax.plot(freqs, left_values, marker='x', linestyle='-', color='r', label='linkes Ohr')
-    ax.invert_yaxis()  # Invert the y-axis as lower dB values indicate better hearing
+    ax.plot(np.log2(freqs), right_values, marker='o', linestyle='-', color='firebrick', label='rechtes Ohr')
+    ax.plot(np.log2(freqs), left_values, marker='x', linestyle='-', color='dodgerblue', label='linkes Ohr')
+
+    # Achsne invertieren und beschriften
+    ax.invert_yaxis()  
     ax.set_title('Audiogramm')
     ax.set_xlabel('Frequenzen (Hz)')
     ax.set_ylabel('Hörschwelle (dB HL)')
     ax.set_ylim(120, -10)
-    ax.set_xscale('log')  # Logarithmic scale for frequencies
-    ax.set_xticks([63, 125, 250, 500, 1000, 2000, 4000, 8000])
-    ax.set_xticklabels(['63', '125', '250', '500', '1000', '2000', '4000', '8000'])
+
+    # Benutzerdefinierte x-Achsen-Ticks
+    x_ticks = np.log2(freq)
+    x_labels = [f"{int(freq)} Hz" for freq in freqs]
+    ax.set_xticks(x_ticks)
+    ax.set_xticklabels(x_labels)
+
+
     ax.set_yticks(np.arange(0, 121, 10))
     ax.grid(True, which='both', linestyle='--', linewidth=0.5)  # Grid for better readability
     ax.legend(loc='upper right')
     
     return fig
 
-# Beispielwerte
-# freq = [63, 125, 250, 500, 1000, 2000, 4000, 8000]
-# dummy_right = [10, 15, 20, 25, 30, 35, 40, 45]
-# dummy_left = [5, 10, 15, 20, 25, 30, 35, 40]
+#Beispielwerte
+freq = [63, 125, 250, 500, 1000, 2000, 4000, 8000]
+dummy_right = [10, 15, 20, 25, 30, 35, 40, 45]
+dummy_left = [5, 10, 15, 20, 25, 30, 35, 40]
 # # Audiogramm erstellen
-# create_audiogram(freq, dummy_right, dummy_left)
+create_audiogram(freq, dummy_right, dummy_left)
+plt.show()
 
