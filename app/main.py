c94f934415ce79916b00aa89e8f08d1b81bba964
Satur42 <sambraunproductions@gmail.com>
2024-06-25 01:00:01 +0200

    Added StandardProcedure class with all funtions to do full audiometer test that is saved in temporary csv file. Also added get_value_from_csv to parent class

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from .ui import setup_ui
 from .model import *
-from .dummy_model import StandardProcedure, TestProcedure
+from .dummy_model import TestProcedure
 from .audiogram import create_audiogram
 
 class Controller():
@@ -8,9 +8,7 @@ class Controller():
     def __init__(self):
         program_functions = {"Klassisches Audiogramm" : self.start_standard_procedure,
                              "Test" : self.start_test_procedure}
-        
         self.familiarization = Familiarization()
-        self.standard_procedure = StandardProcedure()
         self.test_procedure = TestProcedure()
         self.view = setup_ui(self.start_familiarization, 
                              program_functions) 
@@ -21,6 +19,7 @@ class Controller():
         return self.familiarization.familiarize()
 
     def start_standard_procedure(self):
+        self.standard_procedure = StandardProcedure(self.familiarization.get_temp_csv_filename())
         self.standard_procedure.standard_test()
 
     def start_test_procedure(self):
--- app/model.py
+++ app/model.py
@@ -24,6 +24,7 @@ class Procedure():
         self.zero_dbhl = 0.00002 # zero_dbhl in absolute numbers. Needs to be calibrated!
         self.tone_heard = False
         self.freq_bands = ['125', '250', '500', '1000', '2000', '4000', '8000']
+        self.side = 'lr'
 
 
     def dbhl_to_volume(self, dbhl):
@@ -53,7 +54,7 @@ class Procedure():
         """
         self.tone_heard = False
         print("playing tone..")
-        self.ap.play_beep(self.frequency, self.dbhl_to_volume(self.level), self.signal_length, 'l')
+        self.ap.play_beep(self.frequency, self.dbhl_to_volume(self.level), self.signal_length, self.side)
         listener = keyboard.Listener(on_press=self.key_press, on_release=None)
         listener.start()
         current_wait_time = 0
@@ -73,7 +74,8 @@ class Procedure():
     
     def create_temp_csv(self):
         """creates a temporary CSV file with the relevant frequency bands as a header
-        and NaN in the second line as starting value for each band.
+        and NaN in the second and third line as starting value for each band.
+        (second line: left ear, third line: right ear)
 
         Returns:
             str: name of temporary file
@@ -85,30 +87,58 @@ class Procedure():
             # Write header
             csv_writer.writerow(self.freq_bands)
 
-            # Write value NaN for each frequency in second row
+            # Write value NaN for each frequency in second and third row
+            csv_writer.writerow(['NaN' for i in range(len(self.freq_bands))])
             csv_writer.writerow(['NaN' for i in range(len(self.freq_bands))])
 
             return temp_file.name
         
         
-    def add_to_temp_csv(self, value, frequency, temp_filename):
+    def add_to_temp_csv(self, value, frequency, side, temp_filename):
         """add a value in for a specific frequency to the temporary csv file
 
         Args:
             value (str): level in dBHL at specific frequency
             frequency (str): frequency where value should be added
+            side (str): specify which ear ('l' or 'r')
             temp_filename (str): name of temporary csv file
         """
         with open(temp_filename, mode='r', newline='') as temp_file:
             dict_reader = csv.DictReader(temp_file)
-            freq_dict = next(dict_reader)
-            freq_dict[frequency] = value
-            print(freq_dict)
+            freq_dict_l = next(dict_reader)
+            freq_dict_r = next(dict_reader)
+            if side == 'l':
+                freq_dict_l[frequency] = value
+            elif side == 'r':
+                freq_dict_r[frequency] = value
+            print(freq_dict_l, freq_dict_r)
 
         with open(temp_filename, mode='w', newline='') as temp_file:
             dict_writer = csv.DictWriter(temp_file, fieldnames=self.freq_bands)
             dict_writer.writeheader()
-            dict_writer.writerow(freq_dict)
+            dict_writer.writerow(freq_dict_l)
+            dict_writer.writerow(freq_dict_r)
+
+
+    
+    def get_value_from_csv(self, frequency, temp_filename, side='l'):
+        """get the value at a specific frequency from the temporary csv file
+
+        Args:
+            frequency (str): frequency where value is stored
+            temp_filename (str): name of temporary csv file
+            side (str, optional): specify which ear ('l' or 'r'). Defaults to 'l'.
+
+        Returns:
+            str: dBHL value at specified frequency
+        """
+        with open(temp_filename, mode='r', newline='') as temp_file:
+            dict_reader = csv.DictReader(temp_file)
+            freq_dict = next(dict_reader) # left ear
+            if side == 'r': # go to next line if right side
+                freq_dict = next(dict_reader)    
+            return freq_dict[frequency]
+
 
 
 
@@ -170,9 +200,136 @@ class Familiarization(Procedure):
 
             else:
                 print("Familiarization successful!")
-                self.add_to_temp_csv(self.level, '1000', self.tempfile)
+                self.add_to_temp_csv(self.level, '1000', 'l', self.tempfile)
                 return True
 
 
 
+
+class StandardProcedure(Procedure):
+
+    def __init__(self, temp_filename, signal_length=1):
+        """standard audiometer process (rising level)
+
+        Args:
+            temp_filename (str): name of temporary csv file where starting level is stored and future values will be stored
+            signal_length (int, optional): length of played signal in seconds. Defaults to 1.
+        """
+        startlevel = int(self.get_value_from_csv('1000', temp_filename)) - 10 # 10 dB under level from familiarization
+        super().__init__(startlevel, signal_length)
+        self.temp_filename = temp_filename
+        self.freq_order = [1000, 2000, 4000, 8000, 500, 250, 125] # order in which frequencies are tested
+
+    def standard_test(self):
+        """main funtion
+
+        Returns:
+            bool: test successfull
+        """
+
+        self.side = 'l'
+        success_l = self.standard_test_one_ear()
+        self.side = 'r'
+        success_r = self.standard_test_one_ear()
+        if success_l and success_r:
+            return True
+        else:
+            return False
+
+        
+    def standard_test_one_ear(self):
+        """audiometer for one ear
+
+        Returns:
+            bool: test successfull
+        """
+        success = []
+        # test every frequency
+        for f in self.freq_order:
+            print(f"Testing frequeny {f} Hz")
+            s = self.standard_test_one_freq(f)
+            success.append(s)
+
+        # retest 1000 Hz (and more frequencies if discrepancy is too high)
+        for f in self.freq_order:
+            print(f"Retest at frequeny {f} Hz")
+            s = self.standard_test_one_freq(f, retest=True)
+            if s == True:
+                break
+
+        if all(success):
+            return True
+        
+        else:
+            return False
+
+
+    def standard_test_one_freq(self, freq, retest=False):
+        """test for one frequency
+
+        Args:
+            freq (int): frequency at which hearing is tested
+            retest (bool, optional): Is this the retest at the end of step 3 according to DIN. Defaults to False
+
+        Returns:
+            bool: test successfull
+        """
+
+        self.tone_heard = False
+        self.frequency = freq
+        self.level = self.startlevel
+
+        # Step 1 (raise tone in 5 dB steps until it is heard)
+        while self.tone_heard == False:
+            self.play_tone()
+            if self.tone_heard == False:
+                self.level += 5
+
+        # Step 2
+        answers = []
+        tries = 0
+
+        while tries < 10:
+
+            # reduce in 10dB steps until no answer
+            while self.tone_heard == True:
+                self.level -= 10
+                self.play_tone()
+
+            # raise in 5 dB steps until answer
+            while self.tone_heard == False:
+                self.level += 5
+                self.play_tone()
+
+            tries += 1
+            answers.append(self.level)
+            print(f"Try nr {tries}: level: {self.level}")
+
+            if answers.count(self.level) >= 3:
+                if retest == True:
+                    if abs(self.level - int(self.get_value_from_csv(str(self.frequency), self.temp_filename, self.side))) > 5:
+                        self.add_to_temp_csv(str(self.level), str(self.frequency), self.side, self.temp_filename)
+                        return False
+                    else:
+                        self.add_to_temp_csv(str(self.level), str(self.frequency), self.side, self.temp_filename)
+                        return True
+
+                # TODO Wenn Streuung mehr als 10 dB: Vermerk im Audiogramm
+                self.add_to_temp_csv(str(self.level), str(self.frequency), self.side, self.temp_filename)
+                return True
+            
+            # no three same answers in five tries
+            if tries == 5:
+                self.level += 10
+                self.play_tone()
+                answers = []
+
+        print("Something went wrong, please try from the beginning again.")
+        return False
+
+        
+
+
+        
+                
 
