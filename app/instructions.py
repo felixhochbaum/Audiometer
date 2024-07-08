4e4c86dd6d2abb9b17b93f7174670e0f38a1fb3a
Sohaib <Sohaibbct@gmail.com>
2024-07-08 17:14:36 +0200

    adjusted text_Familiarziation

--- app/instructions.py
+++ app/instructions.py
@@ -2,10 +2,11 @@
 
 
 text_Familiarization = """
-\nEs folgt eine kurze Eingewöhnung, in der Sie sich mit dem Programm vertraut machen können.
-Diese Testungen gehen nicht in die späteren Ergebnisse ein und dienen lediglich dazu, zu überprüfen, ob Sie das Verfahren verstanden haben.\n
-Sie werden Tonabfolgen mit unterschiedlichen Lautstärken und Frequenzen hören.
-Bitte drücken Sie jedes Mal, wenn Sie einen Ton wahrnehmen, unverzüglich die Leertaste. 
-Beachten Sie, dass Sie vom Programm keinerlei Feedback erhalten werden – das ist normal."""
+Es folgt eine kurze Eingewöhnung, in der Sie sich mit dem Programm vertraut machen können.
+Diese Testungen gehen nicht in die späteren Ergebnisse ein und dienen lediglich dazu, zu übe-
+rprüfen, ob Sie das Verfahren verstanden haben.
+Sie werden Tonabfolgen mit unterschiedlichen Lautstärken und Frequenzen hören. Bitte drücken
+Sie jedes Mal, wenn Sie einen Ton wahrnehmen, unverzüglich die Leertaste. Beachten Sie, dass
+Sie vom Programm keinerlei Feedback erhalten werden – das ist normal."""
 
 text_calibration = """So geht's."""
