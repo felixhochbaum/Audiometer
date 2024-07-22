14835ac4f7ad9c5e1869cae12e14720b4bb0a676
Satur42 <sambraunproductions@gmail.com>
2024-07-22 17:43:55 +0200

    Controller (main.py) now also has docstrings

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from .model import *
 class Controller():
 
     def __init__(self):
+        """Controller class (MVC architecture) that combines model and view of the Audiometer
+        """
         self.selected_program = ""
         program_functions = {"Klassisches Audiogramm" : self.start_standard_procedure,
                              "Kurzes Screening" : self.start_screen_procedure,
@@ -17,30 +19,71 @@ class Controller():
         self.button_changed = False
     
     def run_app(self):
+        """Starts the app by running the tkinter mainloop of the view
+        """
         self.view.mainloop()
 
     def start_familiarization(self, id="", headphone="Sennheiser_HDA200", calibrate=True, **additional_data):
+        """Creates a Familiarization object and uses it to start the familiarization process
+
+        Args:
+            id (str, optional): ID of test subject. Defaults to "".
+            headphone (str, optional): Name of headphone model being used. Defaults to "Sennheiser_HDA200".
+            calibrate (bool, optional): Whether to use calibration file. Defaults to True.
+
+        Returns:
+            bool: Whether familiarization was successful
+        """
         self.selected_program = "familiarization"
         self.familiarization = Familiarization(id=id, headphone_name=headphone, calibrate=calibrate, **additional_data)
         return self.familiarization.familiarize()
 
     def start_standard_procedure(self, binaural=False, headphone="Sennheiser_HDA200", calibrate=True, **additional_data):
+        """Creates a StandardProcedure object and uses it to start the standard procedure
+
+        Args:
+            binaural (bool, optional): Whether to test both ears at the same time. Defaults to False.
+            headphone (str, optional): Name of headphone model being used. Defaults to "Sennheiser_HDA200".
+            calibrate (bool, optional): Whether to use calibration file. Defaults to True.
+            **additional_data: additional key/value pairs to be stored in CSV file after procedure is done
+        """
         self.selected_program = "standard"
         self.standard_procedure = StandardProcedure(self.familiarization.get_temp_csv_filename(), headphone_name=headphone, calibrate=calibrate, **additional_data)
         self.standard_procedure.standard_test(binaural)
 
     def start_screen_procedure(self, binaural=False, headphone="Sennheiser_HDA200", calibrate=True, **additional_data):
+        """Creates a ScreeningProcedure object and uses it to start the screening procedure
+
+        Args:
+            binaural (bool, optional): Whether to test both ears at the same time. Defaults to False.
+            headphone (str, optional): Name of headphone model being used. Defaults to "Sennheiser_HDA200".
+            calibrate (bool, optional): Whether to use calibration file. Defaults to True.
+            **additional_data: additional key/value pairs to be stored in CSV file after procedure is done
+        """
         self.selected_program = "screening"
         self.screen_procedure = ScreeningProcedure(self.familiarization.get_temp_csv_filename(), headphone_name=headphone, calibrate=calibrate, **additional_data)
         self.screen_procedure.screen_test(binaural)
 
     def start_calibration(self, level, headphone="Sennheiser_HDA200"):
+        """Creates a Calibration object and uses it to start calibration
+
+        Args:
+            level (int): Level of calibration in dB HL.
+            headphone (str, optional): Name of headphone model being used. Defaults to "Sennheiser_HDA200".
+        """
         self.selected_program = "calibration"
         self.calibration = Calibration(startlevel=level, headphone_name=headphone)
         _, current_freq, current_spl = self.calibration_next_freq()
         return current_freq, current_spl
 
     def calibration_next_freq(self):
+        """Go to next frequency in calibration process and play it
+
+        Returns:
+            bool: Whether there are more frequencies left after this one.
+            int: current frequency
+            float: expected SPL value in dB
+        """
         more_freqs, current_freq, current_spl = self.calibration.play_one_freq()
         if more_freqs:
             return True, current_freq, current_spl
@@ -52,15 +95,29 @@ class Controller():
             return False, current_freq, current_spl
 
     def calibration_repeat_freq(self):
+        """Repeat the current frequency during calibration process
+        """
         self.calibration.repeat_freq()
 
     def calibration_set_level(self, spl):
+        """During calbration process, set the level in dB that was measured
+
+        Args:
+            spl (float): Sound pressure level that was measured in dB
+        """
         self.calibration.set_calibration_value(spl)
 
     def stop_sound(self):
+        """Stop the sound during calibration process.
+        """
         self.calibration.stop_playing()
 
     def get_progress(self):
+        """Get current progress in curent procedure for progress bar
+
+        Returns:
+            float: progress value between 0.0 and 1.0
+        """
         if self.selected_program == "familiarization":
             return self.familiarization.get_progress()
         elif self.selected_program == "standard":
